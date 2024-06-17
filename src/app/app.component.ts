aafc8dae45fcce4fac20b5124a6d07bec004c637
sameemul-haque <samhaqk@gmail.com>
2024-06-17 10:02:49 +0530

    update question numbers component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { QuestionNumbersComponent } from './question-numbers/question-numbers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet,NavBarComponent,QuestionNumbersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
