2f705cc34ccc76986dc2cef69dbec3b2c7cb4efa
sameemul-haque <samhaqk@gmail.com>
2024-06-17 10:05:20 +0530

    update question answer comoponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { QuestionNumbersComponent } from './question-numbers/question-numbers.component';
+import { QuestionAnswerComponent } from './question-answer/question-answer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,QuestionNumbersComponent],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    QuestionNumbersComponent,
+    QuestionAnswerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
